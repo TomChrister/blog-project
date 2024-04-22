5603badff2bb7aecdb75a3f530074775e14d1615
Tom Christer Schoon <142898062+TomChrister@users.noreply.github.com>
2024-04-22 20:50:57 +0200

    tested out fetching api's created

--- js/feed.js
+++ js/feed.js
@@ -1,1 +1,41 @@
-test
+const apiArticle = 'https://v2.api.noroff.dev/blog/posts/Tom_Christer';
+const articleDisplay = document.getElementById('articleDisplay');
+
+function append(data) {
+    data.data.forEach((post) => {
+        const div = document.createElement("div");
+        div.classList.add("articles")
+        div.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            ${post.tag ? `<p>Tag: ${post.tag}</p>` : ''}
+            <img src="${post.media.url}" alt="${post.media.alt}">
+            <p>Author: ${post.author.name}</p>
+        `;
+        articleDisplay.appendChild(div);
+        console.log(data);
+    });
+}
+
+
+fetch(apiArticle)
+    .then((response) => response.json())
+    .then((data) => append(data))
+    .catch((error) => {
+        console.error('Error fetching data:', error);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const bearToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9tX0NocmlzdGVyIiwiZW1haWwiOiJ0b21zY2gwMTI2NkBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcxMzM2MDU2M30.Pyo04wxqxm491vDWg9CMi8pug12fM07HWHCkPQjJFak";
