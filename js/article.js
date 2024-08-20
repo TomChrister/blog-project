387ff57449852180cade9ae95b0f4c3729a7d471
Tom Christer Schoon <142898062+TomChrister@users.noreply.github.com>
2024-08-20 17:20:50 +0200

    updated the API to V2

--- js/article.js
+++ js/article.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const articleId = urlParams.get('id');
 const apiArticleById = `https://v2.api.noroff.dev/blog/posts/Tom_Christer/${articleId}`;
-const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9tX0NocmlzdGVyIiwiZW1haWwiOiJ0b21zY2gwMTI2NkBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcxMzM2MDU2M30.Pyo04wxqxm491vDWg9CMi8pug12fM07HWHCkPQjJFak';
+const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9tX0NocmlzdGVyIiwiZW1haWwiOiJ0b21zY2gwMTI2NkBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcyNDE1OTkwM30.vgTqmIt6Cx7hJwmtQYi4tueJrRTHeRjdsobW_xqFXdI';
 
 fetch(apiArticleById)
     .then((response) => response.json())
